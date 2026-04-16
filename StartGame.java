f190ceb7bd116a0ce7e31d7ad6c7085336b59234
Qvistgaarden <tobiasemilqvistgaard@gmail.com>
2026-04-16 14:38:34 +0200

    Initial Commit

---
+++ StartGame.java
@@ -0,0 +1,11 @@
+class StartGame {
+    public static void main(String[] args){
+
+        System.out.println( "Game Start :))" );
+
+        // ToDoList:
+        System.out.println( "TODO: " );
+        System.out.println( "Get Started!" );
+
+    }
+}
